cf95a4b6ba238f252a6b81d25cb38b268f79d892
Jessica <145158102+JessicaY33@users.noreply.github.com>
2023-12-08 15:20:35 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -2,27 +2,27 @@ async function getData()  //returns data from the website
 {
     try{
         //Post Information
-        const response = await fetch('https://jsonplaceholder.typicode.com/users/1/posts');
-        let data = await response.json();
-        console.log(data);  
-        createPost(data);
-    }catch(error){
-        console.log(error);
+        const response = await fetch('https://jsonplaceholder.typicode.com/users/1/posts'); //gets the information from outside
+        let data = await response.json(); //converts to a js object so that the html can return and modify the data
+        console.log(data);  //returns the array data to the console
+        createPost(data); //displays the data on the webpage
+    }catch(error){ //just in case there is an error when displaying the data
+        console.log(error); 
     }
 }
 
 function createPost(obj)  //returns data from the website
 { 
-    let users = obj;
+    let users = obj; //variable holding the data
 
-    const posts = users.map(account =>{
+    const posts = users.map(account =>{ //this creates a post for each element in the array from the data
         return `<div>
         <h2>#${account.id}</h2>
         <h3>${account.title}</h3>
         <p>${account.body}</p>
         </div>`;
     })
-    document.getElementById("Post Board").innerHTML = posts;
+    document.getElementById("Post Board").innerHTML = posts; //Stores the posts into a div to better design the website
 }
 
-getData();
+getData(); //activates the function to collect data and display it
